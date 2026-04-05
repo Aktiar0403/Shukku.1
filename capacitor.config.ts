b8cd09f022a022f4f96573d6aea0098ca496dab2
Aktiar H <aktiardocuments@gmail.com>
2026-04-05 15:51:54 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,11 @@ const config: CapacitorConfig = {
       skipNativeAuth: false,
       providers: ['google.com'],
     },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '553665072411-o357vn2e1e83r9o3pvgr5rgrlos209r3.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
   },
 };
 

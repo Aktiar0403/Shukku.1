9d011f3610e2abec81e112653b68753efabbb4cd
Aktiar H <aktiardocuments@gmail.com>
2026-04-05 10:55:11 +0530

    fgfgh

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,6 @@ import { getFirestore, collection, doc, setDoc, getDoc, updateDoc, deleteDoc, on
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import firebaseConfig from '../firebase-applet-config.json';
 
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);

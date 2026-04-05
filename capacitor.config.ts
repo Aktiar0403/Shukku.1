75c8913fae90ec9bf4a5d737da67a1fe47123db8
Aktiar H <aktiardocuments@gmail.com>
2026-04-05 16:31:52 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,15 +4,15 @@ const config: CapacitorConfig = {
   appId: 'com.shukkuu.app',
   appName: 'Shukkuu',
   webDir: 'dist',
+  server: {
+    url: 'https://shukku-1.vercel.app',
+    cleartext: true,
+    androidScheme: 'https'
+  },
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,
       providers: ['google.com'],
-    },
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      serverClientId: '553665072411-o357vn2e1e83r9o3pvgr5rgrlos209r3.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
     },
   },
 };
